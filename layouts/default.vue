6777e894262df43302dad67cd637d88fa197b9e3
justboereh <justboereh@gmail.com>
2023-07-02 11:43:35 -0500

    migrate project into this repo

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<script setup>
+import '@unocss/reset/tailwind.css'
+
+useHead({
+    htmlAttrs: {
+        style: 'font-size: 12px',
+    },
+})
+</script>
+
+<template>
+    <main>
+        <slot />
+    </main>
+</template>
+
+<style>
+body {
+    font-family: var(--font-family);
+    background-color: var(--surface-ground);
+}
+</style>
